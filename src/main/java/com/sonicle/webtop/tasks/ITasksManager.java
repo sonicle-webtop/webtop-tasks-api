1b99b3014aef992361aff4e5e00a19ca49c5fff3
Matteo Albinola <malbinola@sonicle.com>
2017-02-23 13:40:07 +0100

    Initial import

---
+++ src/main/java/com/sonicle/webtop/tasks/ITasksManager.java
@@ -0,0 +1,63 @@
+/* 
+ * Copyright (C) 2014 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle[dot]com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Copyright (C) 2014 Sonicle S.r.l.".
+ */
+package com.sonicle.webtop.tasks;
+
+import com.sonicle.webtop.core.sdk.WTException;
+import com.sonicle.webtop.tasks.model.Category;
+import com.sonicle.webtop.tasks.model.Task;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author malbinola
+ */
+public interface ITasksManager {
+	
+	public List<Category> listCategories() throws WTException;
+	public Category getCategory(int categoryId) throws WTException;
+	public Category getBuiltInCategory() throws WTException;
+	public Category addCategory(Category cat) throws WTException;
+	public Category addBuiltInCategory() throws WTException;
+	public Category updateCategory(Category cat) throws Exception;
+	public boolean deleteCategory(int categoryId) throws WTException;
+	//public List<CategoryTasks> listTasks(CategoryRoot root, Integer[] categoryFolders, String pattern) throws WTException;
+	//public List<CategoryTasks> listTasks(UserProfile.Id pid, Integer[] categoryFolders, String pattern) throws WTException;
+	public Task getTask(int taskId) throws WTException;
+	public void addTask(Task task) throws WTException;
+	public void updateTask(Task task) throws WTException;
+	public void deleteTask(int taskId) throws WTException;
+	public void deleteTask(ArrayList<Integer> taskIds) throws WTException;
+	public int deleteAllTasks(int categoryId) throws WTException;
+	public void moveTask(boolean copy, int taskId, int targetCategoryId) throws WTException;
+}
---
+++ src/main/java/com/sonicle/webtop/tasks/model/Category.java
@@ -0,0 +1,145 @@
+/* 
+ * Copyright (C) 2014 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle[dot]com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Copyright (C) 2014 Sonicle S.r.l.".
+ */
+package com.sonicle.webtop.tasks.model;
+
+import com.sonicle.webtop.core.sdk.UserProfile;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author malbinola
+ */
+public class Category {
+	public final static String SYNC_OFF = "O";
+	public final static String SYNC_READ_ONLY = "R";
+	public final static String SYNC_READ_WRITE = "W";
+	
+	private Integer categoryId;
+	private String domainId;
+	private String userId;
+	private Boolean builtIn;
+	private String name;
+	private String description;
+	private String color;
+	private String sync;
+	private Boolean isPrivate;
+	private Boolean isDefault;
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getDomainId() {
+		return domainId;
+	}
+
+	public void setDomainId(String domainId) {
+		this.domainId = domainId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Boolean getBuiltIn() {
+		return builtIn;
+	}
+
+	public void setBuiltIn(Boolean builtIn) {
+		this.builtIn = builtIn;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSync() {
+		return sync;
+	}
+
+	public void setSync(String sync) {
+		this.sync = sync;
+	}
+
+	public Boolean getIsPrivate() {
+		return isPrivate;
+	}
+
+	public void setIsPrivate(Boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+
+	public Boolean getIsDefault() {
+		return isDefault;
+	}
+
+	public void setIsDefault(Boolean isDefault) {
+		this.isDefault = isDefault;
+	}
+	
+	public UserProfile.Id getProfileId() {
+		return new UserProfile.Id(getDomainId(), getUserId());
+	}
+	
+	public String getHexColor() {
+		return (StringUtils.indexOf(this.color, "#") == 0) ? StringUtils.substring(this.color, 1) : this.color;
+	}
+}
