08f8135f6695d2836deec042b3336fc151259548
Matteo Albinola <malbinola@sonicle.com>
2023-05-02 11:38:06 +0200

    Move to new folder-sharing API WT-1263

--- src/main/java/com/sonicle/webtop/tasks/model/CategoryFSFolder.java
+++ src/main/java/com/sonicle/webtop/tasks/model/CategoryFSFolder.java
@@ -1,5 +1,5 @@
-/* 
- * Copyright (C) 2014 Sonicle S.r.l.
+/*
+ * Copyright (C) 2023 Sonicle S.r.l.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by
@@ -28,26 +28,30 @@
  * version 3, these Appropriate Legal Notices must retain the display of the
  * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Copyright (C) 2014 Sonicle S.r.l.".
+ * display the words "Copyright (C) 2023 Sonicle S.r.l.".
  */
 package com.sonicle.webtop.tasks.model;
 
-import com.sonicle.webtop.core.model.IncomingShareRoot;
-import com.sonicle.webtop.core.model.SharePermsRoot;
-import com.sonicle.webtop.core.model.ShareRoot;
-import com.sonicle.webtop.core.sdk.UserProfileId;
+import com.sonicle.webtop.core.app.model.FolderShare;
+import com.sonicle.webtop.core.app.model.FolderShareFolder;
 
 /**
  *
  * @author malbinola
  */
-public class ShareRootCategory extends ShareRoot {
+public class CategoryFSFolder extends FolderShareFolder<Integer> {
+	private final Category category;
 	
-	public ShareRootCategory(String shareId, SharePermsRoot perms, UserProfileId ownerProfileId, String description) {
-		super(shareId, perms, ownerProfileId, description);
+	public CategoryFSFolder(int categoryId, FolderShare.Permissions permissions, Category category) {
+		this(categoryId, category.getName(), permissions, category);
 	}
 	
-	public ShareRootCategory(IncomingShareRoot share, SharePermsRoot perms) {
-		super(share, perms);
+	public CategoryFSFolder(int calendarId, String displayName, FolderShare.Permissions permissions, Category category) {
+		super(calendarId, displayName, permissions);
+		this.category = category;
+	}
+	
+	public Category getCategory() {
+		return category;
 	}
 }
--- src/main/java/com/sonicle/webtop/tasks/model/CategoryFSOrigin.java
+++ src/main/java/com/sonicle/webtop/tasks/model/CategoryFSOrigin.java
@@ -1,5 +1,5 @@
-/* 
- * Copyright (C) 2014 Sonicle S.r.l.
+/*
+ * Copyright (C) 2023 Sonicle S.r.l.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by
@@ -28,41 +28,26 @@
  * version 3, these Appropriate Legal Notices must retain the display of the
  * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Copyright (C) 2014 Sonicle S.r.l.".
+ * display the words "Copyright (C) 2023 Sonicle S.r.l.".
  */
 package com.sonicle.webtop.tasks.model;
 
-import com.sonicle.webtop.core.model.ShareFolder;
-import com.sonicle.webtop.core.model.SharePermsFolder;
-import com.sonicle.webtop.core.model.SharePermsElements;
+import com.sonicle.webtop.core.app.model.FolderShare;
+import com.sonicle.webtop.core.app.model.ShareOrigin;
 
 /**
  *
  * @author malbinola
  */
-public class ShareFolderCategory extends ShareFolder {
+public class CategoryFSOrigin extends ShareOrigin {
+	protected final FolderShare.Permissions wildcardPermissions;
 	
-	public ShareFolderCategory(String shareId, SharePermsFolder perms, SharePermsElements elsPerms, Category category) {
-		super(shareId, perms, elsPerms, category);
-	}
-
-	public Category getCategory() {
-		return (Category)object;
+	public CategoryFSOrigin(ShareOrigin origin, FolderShare.Permissions wildcardPermissions) {
+		super(origin);
+		this.wildcardPermissions = wildcardPermissions;
 	}
 	
-	public SharePermsElements getRealElementsPerms(Category.Sync sync) {
-		return realElementsPerms(getElementsPerms(), sync);
-	}
-	
-	public static SharePermsElements realElementsPerms(Category.Sync sync) {
-		return realElementsPerms(SharePermsElements.full(), sync);
-	}
-	
-	public static SharePermsElements realElementsPerms(SharePermsElements origPerms, Category.Sync sync) {
-		if (Category.Sync.READ.equals(sync)) {
-			return new SharePermsElements();
-		} else {
-			return origPerms;
-		}
+	public FolderShare.Permissions getWildcardPermissions() {
+		return wildcardPermissions;
 	}
 }
